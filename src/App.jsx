79a4bc12c3af6064e62b0bc105cdb57f08667916
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-06-07 19:11:40 +0300

    statistic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,107 @@
-import Container from './components/Container/Container'
-import Profile from './components/Profile/Profile'
-import Statistic from './components/Statistic/Statistic'
-import FriendList from './components/FriendList/FriendList'
-
-import user from './data/user.json'
-import statistic from './data/statistic.json'
-import friends from './data/friends.json'
-
-import './App.css'
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <>
-      <Container>
-        <Profile
-          userName={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </Container>
-      <Container>
-        <Statistic title="Upload stats" stats={statistic}></Statistic>
-      </Container>
-      <Container>
-        <Statistic stats={statistic}></Statistic>
-      </Container>
-      <Container>
-        <FriendList friends={friends}></FriendList>
-      </Container>
-    </>
+    <Container>
+      <Wrapper>
+        <Title>Please leave feedback</Title>
+        <List>
+          <Item>
+            <Button type="button">Good</Button>
+          </Item>
+          <Item>
+            <Button type="button">Neutral</Button>
+          </Item>
+          <Item>
+            <Button type="button">Bad</Button>
+          </Item>
+        </List>
+        <Title>Statistic</Title>
+        <List>
+          <Item>
+            <StatisticCell>
+              <CellTitle>Good:</CellTitle>
+              <CellData>99</CellData>
+            </StatisticCell>
+          </Item>
+          <Item>
+            <StatisticCell>
+              <CellTitle>Neutral:</CellTitle>
+              <CellData>99</CellData>
+            </StatisticCell>
+          </Item>
+          <Item>
+            <StatisticCell>
+              <CellTitle>Bad:</CellTitle>
+              <CellData>99</CellData>
+            </StatisticCell>
+          </Item>
+        </List>
+      </Wrapper>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  margin: 0 auto;
+  padding: 0 20px;
+  width: 320px;
+`
+
+const Wrapper = styled.div`
+  padding: 15px;
+  width: 100%;
+  margin-top: 20px;
+  background-color: #dedede;
+  border-radius: 4px;
+  box-shadow: 1px 1px 4px 1px rgba(198, 198, 198, 0.3);
+`
+const Title = styled.h2`
+  padding-bottom: 20px;
+  font-size: 20px;
+  text-align: center;
+`
+
+const List = styled.ul`
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+`
+const Item = styled.li`
+  :not(:last-child) {
+    margin-right: 10px;
+  }
+`
+
+const Button = styled.button`
+  padding: 0 5px;
+  background-color: #d5ffae;
+  border: 1px solid #b4b4b4;
+  border-radius: 4px;
+  box-shadow: 1px 1px 5px 1px rgba(198, 198, 198, 0.6);
+  cursor: pointer;
+  :hover {
+    background-color: #a3f9ff;
+  }
+  :active {
+    background-color: #65f5ff;
+  }
+`
+
+const StatisticCell = styled.div`
+  min-width: 70px;
+  padding: 0 5px;
+  background-color: #fff;
+  border: solid 1px #afafaf;
+  border-radius: 4px;
+`
+
+const CellTitle = styled.h4`
+  font-weight: 400;
+  text-align: center;
+`
+
+const CellData = styled.p`
+  font-weight: 400;
+  text-align: center;
+`
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'modern-normalize/modern-normalize.css'
-import './index.css'
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/typography.css'
+import './styles/index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
