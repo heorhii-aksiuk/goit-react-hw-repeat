8121b1ea9d24e1e5613ee4ca5bf50d55ae95eec6
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-06-29 21:31:02 +0300

    start files and common.css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
 import 'sanitize.css/typography.css'
-import './styles/index.css'
-import App from './components/App/App'
+import './styles/common.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
