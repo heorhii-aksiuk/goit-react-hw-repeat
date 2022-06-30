1922916cd4901d47a2234feeea55c60fb76c3bf4
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-06-30 23:02:23 +0300

    SearchBar c-t styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import SearchBar from './components/SearchBar/SearchBar'
 
 class App extends Component {
   render() {
-    return <div>App</div>
+    return (
+      <Container>
+        <SearchBar />
+      </Container>
+    )
   }
 }
 
 export default App
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`
